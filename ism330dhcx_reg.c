5b470f4b091caf476f819e44e4c5a64397a25c48
Armando Visconti <armando.visconti@st.com>
2025-07-03 14:58:03 +0200

    changed API name into drdy_mask_set/get
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- ism330dhcx_reg.c
+++ ism330dhcx_reg.c
@@ -2754,8 +2754,7 @@ int32_t ism330dhcx_gy_filter_lp1_get(const stmdev_ctx_t *ctx, uint8_t *val)
   * @retval        Interface status (MANDATORY: return 0 -> no Error).
   *
   */
-int32_t ism330dhcx_filter_settling_mask_set(const stmdev_ctx_t *ctx,
-                                            uint8_t val)
+int32_t ism330dhcx_drdy_mask_set(const stmdev_ctx_t *ctx, uint8_t val)
 {
   ism330dhcx_ctrl4_c_t ctrl4_c;
   int32_t ret;
@@ -2782,8 +2781,7 @@ int32_t ism330dhcx_filter_settling_mask_set(const stmdev_ctx_t *ctx,
   * @retval        Interface status (MANDATORY: return 0 -> no Error).
   *
   */
-int32_t ism330dhcx_filter_settling_mask_get(const stmdev_ctx_t *ctx,
-                                            uint8_t *val)
+int32_t ism330dhcx_drdy_mask_get(const stmdev_ctx_t *ctx, uint8_t *val)
 {
   ism330dhcx_ctrl4_c_t ctrl4_c;
   int32_t ret;
